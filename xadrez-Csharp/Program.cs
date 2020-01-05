5dd7811a2425b097294338061e0c838b30dee366
Lgptx <gustavoptx@yahoo.com.br>
2020-01-05 14:45:34 -0300

    First Commit Xadrez Project

---
+++ xadrez-Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xadrez_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
