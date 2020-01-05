3ffa533391de0eefcc95b79f8f01150c6d4c6594
Lgptx <gustavoptx@yahoo.com.br>
2020-01-05 15:57:30 -0300

    Comensando a Implementar o Tabuleiro

--- xadrez-Csharp/Program.cs
+++ xadrez-Csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez_Csharp
 {
@@ -6,7 +7,12 @@ namespace xadrez_Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
+
         }
     }
 }
--- xadrez-Csharp/tabuleiro/Tabuleiro.cs
+++ xadrez-Csharp/tabuleiro/Tabuleiro.cs
@@ -6,5 +6,21 @@ namespace tabuleiro
 {
     class Tabuleiro
     {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha,int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+
     }
 }
