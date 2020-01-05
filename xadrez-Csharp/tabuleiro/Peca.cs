421bfa6eb9ec3a8ed5b91ad6cd70a88fbae9d54d
Lgptx <gustavoptx@yahoo.com.br>
2020-01-05 15:29:10 -0300

    Classes Basicas Incluidas

---
+++ xadrez-Csharp/tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tab = tab;
+            qteMovimentos = 0;
+        }
+    }
+}
