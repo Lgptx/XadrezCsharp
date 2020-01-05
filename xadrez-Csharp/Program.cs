27594377c2c8cb33e00179c1be33916f14c0252d
Lgptx <gustavoptx@yahoo.com.br>
2020-01-05 20:45:57 -0300

    Impressa das cores de pecas

--- xadrez-Csharp/Program.cs
+++ xadrez-Csharp/Program.cs
@@ -8,20 +8,23 @@ namespace xadrez_Csharp
     {
         static void Main(string[] args)
         {
-            try { 
-            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 5));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 7));
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 5));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(1, 7));
 
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
 
             }
-            catch (TabuleiroException e){
-                Console.WriteLine(e.Message);            
-            
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+
             }
 
             Console.ReadLine();
