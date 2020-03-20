0c182b4a806edf3915045da715d43b9032a90ff7
Lgptx <gustavoptx@yahoo.com.br>
2020-03-20 18:50:30 -0300

    bugfix: mudando de nome do metodo moverPara

--- xadrez-Csharp/tabuleiro/Peca.cs
+++ xadrez-Csharp/tabuleiro/Peca.cs
@@ -43,7 +43,7 @@ namespace tabuleiro
 
             return false;
         }
-        public bool podeMoverPara(Posicao pos)
+        public bool movimentoPossivel(Posicao pos)
         {
             return movimentosPossiveis()[pos.linha, pos.coluna];
         }
