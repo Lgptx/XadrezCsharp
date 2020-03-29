ad4810d69ed486a8d7c88fc6309785fa587a5d36
Lgptx <gustavoptx@yahoo.com.br>
2020-03-29 17:41:01 -0300

    Feature: Jogada especial: Promoção

--- xadrez-Csharp/xadrez/PartidaDeXadrez.cs
+++ xadrez-Csharp/xadrez/PartidaDeXadrez.cs
@@ -143,6 +143,21 @@ namespace xadrez
                 desfazMovimnento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em Xeque !");
             }
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial Promocao
+            if (p is Peao)
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+             }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -160,7 +175,7 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-            Peca p = tab.peca(destino);
+            
 
             // #jogadaespecial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
